31c3b2cfcc2dfbbc09c595fedc7a254134224e8d
sumitmaurya777 <sumitmaurya@idsnext.com>
2020-10-10 01:36:43 +0530

    done

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const forecast=require('./utils/Forecast')
 
 const app= express()
 
-const port= process.env.PORT || 3000
+const port= process.env.port || 3000
 
 
 // define path for express config
